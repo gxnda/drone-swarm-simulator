bd287ea06214d5adfc969e08b6b83d08fb7e5889
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2026-06-12 22:08:40 +0100

    feat: implement Drone class with state management and add DroneState enum

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,1 +1,1 @@
-export {};
+export type { DroneId } from "./types/DroneId";
--- packages/simulation/src/drone/Drone.ts
+++ packages/simulation/src/drone/Drone.ts
@@ -1,3 +1,25 @@
-export class Drone {
+import {DroneId} from "@drone-swarm/shared";
+import {Vector3, Quaternion} from "three";
+import {DroneState} from "./DroneState";
 
+export class Drone {
+  readonly id: DroneId;
+  public location: Vector3;
+  public orientation: Quaternion;
+  private state: DroneState;
+
+  constructor(id: DroneId, location: Vector3, orientation?: Quaternion) {
+    this.id = id;
+    this.location = location;
+    this.orientation = orientation ?? new Quaternion();
+    this.state = DroneState.IDLE;
+  }
+
+  public getState(): DroneState {
+    return this.state;
+  }
+
+  public setState(state: DroneState) {
+    this.state = state;
+  }
 }
