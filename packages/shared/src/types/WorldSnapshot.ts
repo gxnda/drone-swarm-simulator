d6a31ae823b7b7b1f16521b8fd440eaa6fa072e7
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2026-06-18 20:33:08 +0100

    chore: tightened up WorldSnapshot.ts

--- packages/shared/src/types/WorldSnapshot.ts
+++ packages/shared/src/types/WorldSnapshot.ts
@@ -3,12 +3,11 @@ import {ISnapshot} from "./ISnapshot";
 import {SerialisedObstacle} from "./SerialisedObstacle";
 
 export class WorldSnapshot extends ISnapshot {
-    public droneSnapshots: DroneSnapshot[];
-    public obstacles: SerialisedObstacle[];
-
-    constructor(droneSnapshots: DroneSnapshot[], obstacles: SerialisedObstacle[]) {
-        super()
-        this.droneSnapshots = droneSnapshots;
-        this.obstacles = obstacles;
-    }
+  constructor(
+    public droneSnapshots: DroneSnapshot[],
+    public obstacles: SerialisedObstacle[],
+    public tick: number
+  ) {
+    super()
+  }
 }
