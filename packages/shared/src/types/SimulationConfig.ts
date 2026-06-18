86fc347f3d3c400a715e2742dc3074b0154ec10e
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2026-06-18 20:32:50 +0100

    chore: replaced worldConfig with simulationConfig

--- packages/shared/src/types/SimulationConfig.ts
+++ packages/shared/src/types/SimulationConfig.ts
@@ -1,2 +1,16 @@
-export class SimulationConfig {
+import {Vector3} from "three";
+import {AlgorithmId} from "./AlgorithmId";
+
+export interface SimulationConfig {
+  readonly droneCount: number;
+  readonly worldSize: Vector3;
+  readonly chunkSize: number;
+  // readonly spawnStrategy: SpawnStrategy;
+  readonly algorithmId: AlgorithmId;
+  // readonly algorithmConfig: AlgorithmConfig;
+  // readonly obstacles: ObstacleConfig[];
+  readonly seed: number;
+  readonly boundsMin: Vector3;
+  readonly boundsMax: Vector3;
+  // readonly physics: PhysicsConfig;
 }
