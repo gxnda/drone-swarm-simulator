5c9dc44c8e366759955ca99a65a74780d2a7e71b
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2026-06-20 17:34:48 +0100

    refactor: change Message class to interface for improved type definition

--- packages/shared/src/types/Message.ts
+++ packages/shared/src/types/Message.ts
@@ -1,13 +1,7 @@
 import {DroneId} from "./DroneId";
 
-export class Message {
-    public text: string;
-    public sender?: DroneId;
-    public recipient: DroneId;
-
-    constructor(text: string, sender: DroneId | null, recipient: DroneId) {
-        this.text = text;
-        if (sender) this.sender = sender;
-        this.recipient = recipient;
-    }
+export interface Message {
+    text: string;
+    sender?: DroneId;
+    recipient: DroneId;
 }
