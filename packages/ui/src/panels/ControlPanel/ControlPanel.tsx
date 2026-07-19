f9b4229f792f1e5b87daae7a502bb51d8be4ddb3
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2026-07-19 18:37:49 +0100

    chore: added placeholder control panel + dependancies

---
+++ packages/ui/src/panels/ControlPanel/ControlPanel.tsx
@@ -0,0 +1,17 @@
+
+function ControlPanel(
+  isRunning: boolean,
+  speedMultiplier: number,
+  currentTick: number,
+  droneCount: number,
+  onPlay: () => void,
+  onPause: () => void,
+  onReset: () => void,
+  onStep: () => void,
+  onSpeedChange: (newMultiplier: number) => void,
+  onDroneCountChange: (newDroneCount: number) => void,
+) {
+
+}
+
+export default ControlPanel;
